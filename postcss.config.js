90689bbd2adf0a60fb896aa4ea0ae36ba6b9436c
adrianofinco <adriano.finco.509@gmail.com>
2021-05-22 15:00:13 -0300

    fix: fix cssnano integration

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,10 @@ const tailwind = require('tailwindcss');
 const cssnano = require('cssnano');
 
 module.exports = {
-  plugins: process.env.NODE_ENV == 'production'
-            ? [tailwind, cssnano({ preset: 'default', discardComments: {removeAll: true} })] 
-            : [tailwind]
+  plugins: [
+    tailwind({}),
+    process.env.NODE_ENV.trim() == 'production' 
+      ? cssnano({ preset: 'default', discardComments: {removeAll: true} }) :
+      null
+  ]
 }
